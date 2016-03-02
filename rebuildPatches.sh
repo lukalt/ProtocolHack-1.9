2c8a81afe026423f7157d83e20745e4ff74c9143
Lukas <lukas81298@gmail.com>
2016-03-02 15:00:31 +0100

    Update rebuildPatches.sh

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -41,5 +41,5 @@ function savePatches {
 
 #savePatches Bukkit Spigot-API
 #savePatches CraftBukkit Spigot-Server
-savePatches Spigot-API PaperSpigot-API
-savePatches Spigot-Server PaperSpigot-Server
+savePatches Spigot-API ProtocolHackSpigot-API
+savePatches Spigot-Server ProtocolHackSpigot-Server
